a58aba073cf3f01b28761d6f037ff2c569a86a0d
Jigyass <90003678+Jigyass@users.noreply.github.com>
2023-10-05 18:35:51 -0500

    Added file 4/13

---
+++ footer.php
@@ -0,0 +1,18 @@
+
+</div>
+<br>
+
+
+<style>
+    footer{
+        background: black;
+        color: white;
+    }
+</style>
+<footer class="container-fluid text-center bg-dark">
+        <p>@ 2023</p>
+    </footer>
+
+</body>
+
+</html>
